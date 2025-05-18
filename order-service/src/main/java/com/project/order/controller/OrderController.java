2c05cbb67c15437852f9df14c29b3eb98192cf57
YHN <skygksmf1031@naver.com>
2025-05-18 18:55:10 +0900

    feat: [019-order-service] 시장가 주문 API 구현
    - /orders/market POST API 추가
    - MarketOrderService 구현
    - 주문 엔티티 및 상태/유형 ENUM 정의
    - 임시 시장가 하드코딩 적용 (추후 price-service 연동 예정)

---
+++ order-service/src/main/java/com/project/order/controller/OrderController.java
@@ -0,0 +1,47 @@
+package com.project.order.controller;
+
+import com.project.order.dto.OrderRequest;
+import com.project.order.entity.Order;
+import com.project.order.service.LimitOrderService;
+import com.project.order.service.MarketOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 시장가 주문 API 컨트롤러
+ *
+ * @author ㅇㅇ
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+
+  private final MarketOrderService marketOrderService;
+  private final LimitOrderService limitOrderService;
+
+  /**
+   * 시장가 주문을 생성합니다.
+   *
+   * @param request - 주문 요청 정보
+   * @return 생성된 주문 정보
+   */
+  @PostMapping("/market")
+  public ResponseEntity<Order> placeMarketOrder(@RequestBody OrderRequest request) {
+    Order order = marketOrderService.placeMarketOrder(request);
+    return ResponseEntity.ok(order);
+  }
+
+  /**
+   * 지정가 주문을 생성합니다.
+   *
+   * @param request - 주문 요청 정보
+   * @return 생성된 주문 정보
+   */
+  @PostMapping("/limit")
+  public ResponseEntity<Order> placeLimitOrder(@RequestBody OrderRequest request) {
+    Order order = limitOrderService.placeLimitOrder(request);
+    return ResponseEntity.ok(order);
+  }
+}
