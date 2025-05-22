8374d3cd940e9ef9ec9131265654074100f0024c
HUIJAEKO <xrhgmlwox@naver.com>
2025-05-22 10:46:38 +0900

    feat: [023-Portfolio-Environment-Setting] 포트폴리오 모듈 실행을 위한 환경 세팅

---
+++ portfolio-service/src/main/java/com/project/portfolio/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.project.portfolio.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Auth Service API",
+                version = "v1.0.0",
+                description = "포트폴리오 API 문서",
+                contact = @Contact(
+                        name = "고희재",
+                        email = "rhgmlwo11@gmail.com"
+                )
+        )
+)
+@Configuration
+public class OpenApiConfig {
+}
+
