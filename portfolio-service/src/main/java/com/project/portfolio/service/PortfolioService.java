d4d7c83af8da60b4cd8165c71f773b131447d6bb
HUIJAEKO <xrhgmlwox@naver.com>
2025-05-22 11:59:49 +0900

    feat: [008-Get-All-Portfolios] 사용자의 포트폴리오 목록 조회

--- portfolio-service/src/main/java/com/project/portfolio/service/PortfolioService.java
+++ portfolio-service/src/main/java/com/project/portfolio/service/PortfolioService.java
@@ -1,6 +1,7 @@
 package com.project.portfolio.service;
 
 import com.project.portfolio.dto.PortfolioCreationDto;
+import com.project.portfolio.dto.PortfolioResponseDto;
 import com.project.portfolio.entity.Portfolio;
 import com.project.portfolio.repository.PortfolioRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +26,13 @@ public class PortfolioService {
         portfolio.setCreatedAt(LocalDateTime.now());
 
         portfolioRepository.save(portfolio);
+    }
+
+    // 사용자의 포트폴리오 전체 조회
+    @Transactional(readOnly = true)
+    public List<PortfolioResponseDto> getSimplePortfolios(Long userId) {
+        return portfolioRepository.findAllByUserId(userId).stream()
+                .map(p -> new PortfolioResponseDto(p.getPortfolioId(), p.getName()))
+                .toList();
     }
 }
