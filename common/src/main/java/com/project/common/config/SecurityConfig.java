2b855c8f3ddac8a30607990ff9ae12b9debb9ec6
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-06-28 16:15:22 +0900

    refactor

--- common/src/main/java/com/project/common/config/SecurityConfig.java
+++ common/src/main/java/com/project/common/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointR
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
